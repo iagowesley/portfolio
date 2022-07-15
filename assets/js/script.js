93da0f292a72fb7b9bb7b5151024d1da5431da01
iago.costa <iago.costa@fitbank.com.br>
2022-07-15 10:17:55 -0300

    scroll animate

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,6 +24,8 @@ menuLinks.forEach((link) => {
 	link.addEventListener("click", scrollToSection);
 }); */
 
+
+/*
 const observer = new IntersectionObserver(entries => {
 	console.log(entries)
 	entries[0].target.classList.add('.init-hidden-off')
@@ -33,4 +35,30 @@ const observer = new IntersectionObserver(entries => {
 
 Array.from(document.querySelectorAll('.init-hidden')).forEach(element => {
 	observer.observe(element)
+})*/
+
+const target = document.querySelectorAll('[data-anime');
+const animationClass = 'animate';
+
+function animeScroll() {
+	const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+	target.forEach(function (element) {
+		if ((windowTop) > element.offsetTop) {
+			element.classList.add(animationClass);
+		} else {
+			element.classList.remove(animationClass);
+		}
+})
+}
+
+animeScroll();
+
+if(target.length){
+	window.addEventListener('scroll', function () {
+		animeScroll();
+	});
+}
+
+window.addEventListener('scroll', function() {
+	animeScroll();
 })
